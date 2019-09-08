33868b80d4c499c9ab898f29a504d9d9ed0c29a9
Mike <mpawlak719@gmail.com>
2019-09-07 23:37:54 -0500

    add cors

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const profile = require("./modules/profile");
 //   cache.setProfile(profile);
 // });
 
+app.all("/", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(helmet());
 
 app
